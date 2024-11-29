bce4678c7c05297b5c71fb17442cf33bbd6c39f2
Ibrahim Goumrane <goumrane.ibrahim@ensam-casa.ma>
2024-11-29 16:41:54 +0000

    add custom fonts and extend Tailwind CSS theme with new font families, sizes, and colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,29 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        HubotSans: ['HubotSans', 'sans-serif'],
+        PinyonScript: ['Pinyon Script', 'sans-serif'],
+      },
+      fontSize: {
+        nav: '32px',
+        mainTitle: '64px',
+        secondayTitle: '50px',
+        mainParagraph: '40px',
+        secondaryParagraph: '32px',
+      },
+      colors: {
+        white: {
+          main: '#FFFFFF',
+          secondary: '#F3F0E9',
+        },
+        black: {
+          main: '#292929',
+          secondary: '#000000',
+        },
+      },
+    },
   },
   plugins: [],
 };
