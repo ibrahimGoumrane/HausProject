5d0df2dc8f12b6c837e946c2c0981a8d8e7973e9
Oussama Darrazi <nostra.darrazi2002@gmail.com>
2024-12-15 10:03:16 +0100

    added preline's reinitialization helper

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,23 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import RootLayout from './Layout/main';
 import Home from './Pages/Home';
 import Error from './Pages/Error';
+import { useLocation } from 'react-router-dom';
+import 'preline/preline';
+import { IStaticMethods } from 'preline/preline';
+import { useEffect } from 'react';
+//reinitializes the components every time when app is mounted or page was changed
+//https://preline.co/docs/frameworks-react.html
+declare global {
+  interface Window {
+    HSStaticMethods: IStaticMethods;
+  }
+}
 
 function App() {
+  const location = useLocation();
+  useEffect(() => {
+    window.HSStaticMethods.autoInit();
+  }, [location.pathname]);
   return (
     <BrowserRouter>
       <Routes>
