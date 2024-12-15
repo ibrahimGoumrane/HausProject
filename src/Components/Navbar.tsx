2e7eb5da62dc0e65aca5d86266fe7fb59a1ab44d
Oussama Darrazi <nostra.darrazi2002@gmail.com>
2024-12-15 18:37:22 +0100

    refactored - collapsable Navbar + padding + drop down

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -1,35 +1,141 @@
 import { Link } from 'react-router-dom';
-import DropDown from './DropDown';
 import NavItem from './NavItem';
 
-const Navbar = () => {
+export default function Navbar() {
   return (
-    <nav className="fixed top-0 left-0 w-full bg-white  pt-8 pb-2 px-8 flex items-center z-50 justify-between border-b-[2px] border-black-primary bg-white-primary">
-      <Link to="/" className="flex items-center gap-3">
-        <span className="text-title-primary font-HubotSans font-bold text-black-main">Haus.</span>
-      </Link>
-      <div className="flex items-center gap-8">
-        <NavItem to="#about" text="About Us" />
-        <NavItem to="#blog" text="Blog" />
-        <NavItem to="#faq" text="FAQ" />
-        <NavItem to="#contact" text="Contact Us" />
-      </div>
-      <div className="flex items-center gap-6">
-        <DropDown
-          options={['Book', 'Cancel']}
-          onClick={(value) => {
-            console.log(value);
-          }}
-        />
-        <Link
-          to="/login"
-          className="w-[150px] h-[75px] gap-2 flex items-center border-2 border-black-primary justify-center text-black-primary text-nav"
-        >
-          <span>Login</span>
-        </Link>
-      </div>
-    </nav>
-  );
-};
+    // <!-- ========== HEADER ========== -->
+    <header className="sticky top-0 flex flex-wrap md:justify-start md:flex-nowrap z-50 w-full border-b-2 border-black-primary bg-white-primary">
+      <nav className="relative  w-full flex flex-wrap md:grid md:grid-cols-12 basis-full items-center px-8 py-2 mx-auto">
+        <div className="md:col-span-3">
+          {/* <!-- Logo --> */}
+          <Link to="/" className="flex items-center gap-3">
+            <span className="text-title-primary font-HubotSans font-bold text-black-main">Haus.</span>
+          </Link>
+          {/* <!-- End Logo --> */}
+        </div>
 
-export default Navbar;
+        {/* <!-- Button Group --> */}
+        <div className="flex items-center gap-x-2 md:gap-x-4 ms-auto  md:order-3 md:col-span-3">
+          {/* Drop Down */}
+          <div className="hs-dropdown relative inline-flex">
+            <button
+              id="hs-dropdown-slideup-animation"
+              type="button"
+              className="hs-dropdown-toggle border-2 border-black-primary py-1 px-4 inline-flex items-center gap-x-2 text-nav font-medium bg-black-primary text-white-primary  disabled:pointer-events-none"
+              aria-haspopup="menu"
+              aria-expanded="false"
+              aria-label="Dropdown"
+            >
+              Book
+              <svg
+                className="hs-dropdown-open:rotate-180 transition-none"
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              >
+                <path d="m6 9 6 6 6-6" />
+              </svg>
+            </button>
+
+            <div
+              className="hs-dropdown-menu transition-[opacity,margin] duration hs-dropdown-open:opacity-100 opacity-0 hidden z-10 duration-300 mt-2 min-w-60 bg-white-primary"
+              role="menu"
+              aria-orientation="vertical"
+              aria-labelledby="hs-dropdown-slideup-animation"
+            >
+              <div className=" bg-white-primary border-2 border-black-primary">
+                <a
+                  className="text-nav flex items-center  py-2 px-3 text-black-primary hover:bg-black-primary hover:text-white-primary"
+                  href="#"
+                >
+                  A Hive
+                </a>
+                <a
+                  className="text-nav flex items-center py-2 px-3  text-black-primary hover:bg-black-primary hover:text-white-primary"
+                  href="#"
+                >
+                  A Meeting Room
+                </a>
+              </div>
+            </div>
+          </div>
+          {/* End Drop Down */}
+          <Link
+            to="/login"
+            className="py-1 px-4 gap-2 flex items-center border-2 border-black-primary justify-center text-black-primary hover:bg-black-primary hover:text-white-primary text-nav"
+          >
+            <span>Login</span>
+          </Link>
+
+          <div className="md:hidden">
+            <button
+              type="button"
+              className="hs-collapse-toggle size-10 flex justify-center items-center text-black-primary disabled:opacity-50 disabled:pointer-events-none"
+              id="hs-navbar-hcail-collapse"
+              aria-expanded="false"
+              aria-controls="hs-navbar-hcail"
+              aria-label="Toggle navigation"
+              data-hs-collapse="#hs-navbar-hcail"
+            >
+              <svg
+                className="hs-collapse-open:hidden shrink-0 size-4"
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              >
+                <line x1="3" x2="21" y1="6" y2="6" />
+                <line x1="3" x2="21" y1="12" y2="12" />
+                <line x1="3" x2="21" y1="18" y2="18" />
+              </svg>
+              <svg
+                className="hs-collapse-open:block hidden shrink-0 size-4"
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              >
+                <path d="M18 6 6 18" />
+                <path d="m6 6 12 12" />
+              </svg>
+            </button>
+          </div>
+        </div>
+        {/* <!-- End Button Group --> */}
+
+        {/* <!-- Collapse --> */}
+        <div
+          id="hs-navbar-hcail"
+          className="hs-collapse hidden overflow-hidden transition-all duration-300 basis-full grow md:block md:w-auto md:basis-auto md:order-2 md:col-span-6"
+          aria-labelledby="hs-navbar-hcail-collapse"
+        >
+          <div className="flex flex-col gap-y-10 gap-x-0 mt-5 md:flex-row md:justify-center md:items-center md:gap-y-0 md:gap-x-7 md:mt-0">
+            <NavItem to="#about" text="About Us" />
+            <NavItem to="#blog" text="Blog" />
+            <NavItem to="#faq" text="FAQ" />
+            <NavItem to="#contact" text="Contact Us" />
+          </div>
+        </div>
+        {/* <!-- End Collapse --> */}
+      </nav>
+    </header>
+    // <!-- ========== END HEADER ========== -->
+  );
+  //   <!-- ========== END HEADER ========== -->;
+}
---
+++ src/Components/__Navbar.tsx
@@ -0,0 +1,35 @@
+import { Link } from 'react-router-dom';
+import DropDown from './DropDown';
+import NavItem from './NavItem';
+
+const __Navbar = () => {
+  return (
+    <nav className="fixed top-0 left-0 w-full bg-white  py-2 px-8 flex items-center z-50 justify-between border-b-[2px] border-black-primary bg-white-primary">
+      <Link to="/" className="flex items-center gap-3">
+        <span className="text-title-primary font-HubotSans font-bold text-black-main">Haus.</span>
+      </Link>
+      <div className="flex items-center gap-8">
+        <NavItem to="#about" text="About Us" />
+        <NavItem to="#blog" text="Blog" />
+        <NavItem to="#faq" text="FAQ" />
+        <NavItem to="#contact" text="Contact Us" />
+      </div>
+      <div className="flex items-center gap-6">
+        <DropDown
+          options={['Book', 'Cancel']}
+          onClick={(value) => {
+            console.log(value);
+          }}
+        />
+        <Link
+          to="/login"
+          className="w-[150px] h-[75px] gap-2 flex items-center border-2 border-black-primary justify-center text-black-primary text-nav"
+        >
+          <span>Login</span>
+        </Link>
+      </div>
+    </nav>
+  );
+};
+
+export default __Navbar;
